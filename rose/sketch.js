474c3ad4d193b871bc2da19aed0c0d824e4034ac
moritz-di <moritz.zuest@stud.hslu.ch>
2022-04-06 23:37:15 +0200

    -

--- rose/sketch.js
+++ rose/sketch.js
@@ -6,24 +6,30 @@ console.log(numb);
 let canvasSize = 800;
 let r = 100;
 let noiseScale = 0.02;
+let flowerMask;
+let rNoise;
 
 function setup(){
   createCanvas(canvasSize, canvasSize, SVG);
   strokeWeight(3);
   background('#172231');
   angleMode(DEGREES);
+  rectMode(CENTER);
+  stroke('rgba(188, 0, 254,0.1)');
+  noFill();
 
-  let flowerMask = createGraphics(canvasSize, canvasSize);
-
-  // flowerMask.stroke('rgba(188, 0, 254,1)');
+  // flowerMask = createGraphics(canvasSize, canvasSize);
+  // flowerMask.stroke('rgba(188, 0, 254, 1)');
+  // flowerMask.noFill();
+  // flowerMask.strokeWeight(3);
+  // flowerMask.angleMode(DEGREES);
   // flowerMask.beginShape();
-  // for (let angle = 0; angle < 900; angle += 10){
-  //   rNoise = noise(noiseScale) * 30;
+  // for (let angle = 0; angle < 360; angle += 10){
+  //   rNoise = noise(noiseScale) * 700;
   //   flowerMask.curveVertex(cos(angle) * rNoise, sin(angle) * rNoise);
   //   noiseScale = noiseScale + 0.01;
   // }
-  // flowerMask.endShape();
-
+  // flowerMask.endShape(CLOSE);
 
 }
 
@@ -33,7 +39,7 @@ function draw(){
   noStroke();
   rect(0, 0, canvasSize, canvasSize)
   drawFlower();
-  //flower().mask(flowerMask);
+  // image(flowerMask, 0, 0)
   //save("mySVG.svg");
   noLoop();
 }
@@ -47,6 +53,7 @@ function drawFlower(){
   // set center
   translate(width/2, height/2);
 
+  // light circle
   let leavesBg = random(50, 100);
 
   for(let i = 0; i < leavesBg; i++){
@@ -62,13 +69,14 @@ function drawFlower(){
     endShape();
   }
 
+  // bold circle
   let leavesSm = 1;
   for(let i = 0; i < leavesSm; i++){
 
     drawingContext.shadowBlur = 10;
     drawingContext.shadowColor = 'rgba(255, 255, 255, .3)';
   
-    stroke('rgba(188, 0, 254,0.8)');
+    stroke('rgba(188, 0, 254,.8)');
     r = random(300, 400);
     beginShape();
     let pieceSizeSm = random(4000, 6000);
@@ -80,7 +88,8 @@ function drawFlower(){
     endShape();
   }
 
-  stroke('rgba(188, 0, 254,1)');
+  // inner circle
+  stroke('rgba(188, 0, 254,.8)');
   beginShape();
   for (let angle = 0; angle < 900; angle += 10){
     rNoise = noise(noiseScale) * 30;
