d5bba8ec0d9da5018d43ddf273b2496b925860e5
dariobossy <dario.bossy@stud.hslu.ch>
2022-04-07 10:55:30 +0200

    Update scrolltrigger.js

--- assets/js/scrolltrigger.js
+++ assets/js/scrolltrigger.js
@@ -1,9 +1,9 @@
-gsap.set(".section-1 .flower-wrapper > div", {scale: "0.5", rotate: "460", yPercent: 0}),
+gsap.set(".section-1 .flower-wrapper > div", {scale: "0", rotate: "460", yPercent: 0}),
 gsap.to(".section-1 .flower-wrapper > div", {scale: "1", rotate: "0", scrollTrigger: {
       trigger: ".section-1",
-      start: "top top",
-      end: "+=1200",
+      start: "top bottom",
+      end: "bottom",
       //markers:true,
-      pin: true,
-      scrub: true
+      //pin: true,
+      scrub: 1
 }})
